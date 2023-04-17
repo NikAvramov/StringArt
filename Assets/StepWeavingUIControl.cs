791063b1ae719bac9ec597d3a211cab23391cbc1
Николай Аврамов <nikavramov2015@gmail.com>
2023-04-17 15:18:15 +0300

    заработала кнопка сохранить прогресс, приложение собрано

--- Assets/StepWeavingUIControl.cs
+++ Assets/StepWeavingUIControl.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using TMPro;
 using UnityEngine;
 
@@ -7,31 +9,45 @@ public class StepWeavingUIControl : MonoBehaviour
 {
   public List<NodesMap> nodes;
   public NodesMap currentNode;
+
   public TMP_Text step;
   public TMP_Text node;
   public TMP_Text nextNode;
   public TMP_Text schemaName;
+
   public float widthLine;
   public Material material;
   public GameObject currentLine;
+
   public TMP_Text pointPrefab;
   public TMP_Text activPoint;
   public TMP_Text endPoint;
   public Transform canvas;
   private GameObject[] lines;
+
   void Start()
   {
-    currentNode = nodes.FirstOrDefault(n => n.IsReady == false);
+    lines = new GameObject[nodes.Count];
+    activPoint = Instantiate(pointPrefab, canvas);
+    endPoint = Instantiate(pointPrefab, canvas);
+    widthLine = nodes[0].Width;
+
+    currentNode = nodes[0];
+    int i = 0;
+    while(currentNode.IsReady == true && i != nodes.Count - 1) 
+    {
+      i++;
+      DrawLine();
+      currentNode = nodes[i];
+    }
+
+
+    //currentNode = nodes.FirstOrDefault(n => n.IsReady == false);
   
     step.text = (currentNode.IDstep + 1).ToString();
     node.text = currentNode.IDnode.ToString();
     nextNode.text = nodes.FirstOrDefault(n => n.IDstep == currentNode.IDstep + 1).IDnode.ToString();
-    widthLine = nodes[0].Width;
 
-    lines = new GameObject[nodes.Count];
-
-    activPoint = Instantiate(pointPrefab, canvas);
-    endPoint = Instantiate(pointPrefab, canvas);
     DrawLine();
   }
   public void DrawLine()
@@ -68,11 +84,12 @@ public class StepWeavingUIControl : MonoBehaviour
   }
   public void PreviousStep()
   {
-    if(currentNode.IDstep > 0)
+    nodes[currentNode.IDstep].IsReady = false;
+    if (currentNode.IDstep > 0)
     {
-      nodes[currentNode.IDstep].IsReady = false;
       Destroy(lines[currentNode.IDstep]);
       lines[currentNode.IDstep] = null;
+
       currentNode = nodes.FirstOrDefault(n => n.IDstep == currentNode.IDstep - 1);
       step.text = (currentNode.IDstep + 1).ToString();
       node.text = currentNode.IDnode.ToString();
@@ -86,5 +103,16 @@ public class StepWeavingUIControl : MonoBehaviour
       endPoint.transform.position = end;
       endPoint.text = nextPointNode.IDnode.ToString();
     }
+  }
+  public void SaveProgress()
+  {
+    var path = Application.persistentDataPath + $"/{schemaName.text}.schema";
+    if (File.Exists(path))
+    {
+      File.Delete(path);
+    }
+    using var fs = new FileStream(path, FileMode.OpenOrCreate);
+    var bf = new BinaryFormatter();
+    bf.Serialize(fs, nodes);
   }
 }
